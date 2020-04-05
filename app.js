adf2c4e85e8e8a2c5aa1ebd7dd21cc45bfc16f78
Sheila Anguiano <sheila.anguiano@gmail.com>
2020-04-04 21:27:21 -0700

    updated /quotes route

--- app.js
+++ app.js
@@ -1,16 +1,18 @@
 const express = require('express');
 const app = express();
 
+const records = require('./records');
 
-//Send a GET request to /phrases to READ a list of quotes
-app.get('/phrases', (req, res)=>{
-    res.json(data);
+
+//Send a GET request to /quotes to READ a list of quotes
+app.get('/quotes', async (req, res)=>{
+    const quotes = await records.getQuotes();
+    res.json(quotes);
 });
 
 //Send a GET request to /quotes/:id to READ(view) a quote
-app.get('/phrases/:id', (req, res)=>{
-    const phrase = data.phrases.find(phrase => phrase.id == req.params.id);
-    res.json(phrase);
+app.get('/quotes/:id', (req, res)=>{
+    
 });
 
 //Send a POST request to /quotes  to CREATE a new quote
@@ -20,22 +22,3 @@ app.get('/phrases/:id', (req, res)=>{
 
 app.listen(3000, () => console.log('Quote API listening on port 3000!'));
 
-const data = {
-    phrases: [
-      {
-        id: 8721,
-        phrase: "Ad astra per aspera.",
-        meaning: "Through adversity to the stars"
-      },
-      {
-        id: 5779,
-        phrase: "Acta non verba.",
-        meaning: "Deeds, not words"
-      },
-      {
-        id: 3406,
-        phrase: "Aut viam inveniam aut faciam",
-        meaning: "I will either find a way or make one,"
-      }
-    ]
-  }
