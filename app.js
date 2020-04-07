b96bdefe2c2b44645f72389f5ed33ad41b0b5ef6
Sheila Anguiano <sheila.anguiano@gmail.com>
2020-04-06 22:15:11 -0700

    POST route

--- app.js
+++ app.js
@@ -3,19 +3,46 @@ const app = express();
 
 const records = require('./records');
 
+app.use(express.json());
+
 
 //Send a GET request to /quotes to READ a list of quotes
-app.get('/quotes', async (req, res)=>{
-    const quotes = await records.getQuotes();
-    res.json(quotes);
-});
+app.get('/quotes', async (req, res) => {
+    try {
+        const quotes = await records.getQuotes();
+        res.json(quotes);
 
-//Send a GET request to /quotes/:id to READ(view) a quote
-app.get('/quotes/:id', (req, res)=>{
+    } catch(err) {
+        res.json({message: err.message});
+    }
     
 });
 
+//Send a GET request to /quotes/:id to READ(view) a quote
+app.get('/quotes/:id', async (req, res) => {
+    try {
+        const quote = await records.getQuote(req.params.id);
+        res.json(quote);
+
+    } catch(err){
+        res.json({message: err.message});
+    }  
+});
+
 //Send a POST request to /quotes  to CREATE a new quote
+app.post('/quotes', async (req, res) => {
+    try{
+        const quote = await records.createQuote({
+            quote: req.body.quote,
+            author: req.body.author
+        });   
+        res.json(quote);
+
+    } catch(err) {
+        res.json({message: err.message});
+    }
+});
+
 //Send a PUT request to /quotes/:id to UPDATE(edit) a quote
 //Send a DELETE request to/quotes/:id  to DELETE a quote
 //Send a GET request to /quotes/quote/random READ (view) a random quote
